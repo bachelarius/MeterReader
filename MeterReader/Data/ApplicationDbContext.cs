a00abc68b25fe8d2f38027bef2466ca8ecc6a557
Nikolay Timofeev <nikolay@timofeev.co.uk>
2025-04-01 22:38:03 +0100

    Add Account table to database

--- MeterReader/Data/ApplicationDbContext.cs
+++ MeterReader/Data/ApplicationDbContext.cs
@@ -1,12 +1,19 @@
+using MeterReaderAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace MeterReaderAPI.Data;
 
-public class ApplicationDbContext : DbContext {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options) {}
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+    : DbContext(options) {
+    public DbSet<Account> Accounts { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) {
         base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Account>(entity => {
+            entity.HasKey(e => e.AccountId);
+            entity.Property(e => e.FirstName).IsRequired();
+            entity.Property(e => e.LastName).IsRequired();
+        });
     }
 }
