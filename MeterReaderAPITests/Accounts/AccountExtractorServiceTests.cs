c72cf80680a4dfa3ee93cc02d68b7ed7eb6a03ec
Nikolay Timofeev <nikolay@timofeev.co.uk>
2025-04-02 04:06:12 +0100

    add simple unit test for AccountExtractorService

---
+++ MeterReaderAPITests/Accounts/AccountExtractorServiceTests.cs
@@ -0,0 +1,20 @@
+using MeterReaderAPI.Accounts;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace MeterReaderAPITests.Accounts;
+
+public class AccountExtractorServiceTests
+{
+    [Fact]
+    public void ExtractAccounts_WithValidCsv_ShouldReturnAccounts()
+    {
+        var sut = new AccountExtractorService(Mock.Of<ILogger<AccountExtractorService>>());
+
+        // Act
+        var accounts = sut.ExtractAccounts().ToList();
+
+        // Assert
+        Assert.Equal(27, accounts.Count);
+    }
+}
