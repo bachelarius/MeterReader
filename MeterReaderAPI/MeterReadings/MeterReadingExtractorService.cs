4073109f17774a34e98232100dc740233b44f58c
Nikolay Timofeev <nikolay@timofeev.co.uk>
2025-04-02 04:59:46 +0100

    first pass at meter reading csv parser

--- MeterReaderAPI/MeterReadings/MeterReadingExtractorService.cs
+++ MeterReaderAPI/MeterReadings/MeterReadingExtractorService.cs
@@ -1,13 +1,23 @@
-﻿namespace MeterReaderAPI.MeterReadings {
+﻿using FileHelpers;
+using MeterReaderAPI.Accounts;
+using Microsoft.Extensions.Logging;
+
+namespace MeterReaderAPI.MeterReadings {
     
     public interface IMeterReadingExtractorService {
-        IEnumerable<MeterReadingCsvDTO> ExtractMeterReadings();
+        IEnumerable<MeterReadingCsvDTO> ExtractMeterReadings(string csvFileContent);
     }
 
+    public class MeterReadingExtractorService(ILogger<MeterReadingExtractorService> logger) : IMeterReadingExtractorService {
+        private readonly ILogger<MeterReadingExtractorService> logger = logger;
 
-    public class MeterReadingExtractorService : IMeterReadingExtractorService {
-        public IEnumerable<MeterReadingCsvDTO> ExtractMeterReadings() {
-            throw new NotImplementedException();
+        public IEnumerable<MeterReadingCsvDTO> ExtractMeterReadings(string csvFileContent) {
+            logger.LogDebug("Parsing stream as csv file");
+            var engine = new FileHelperEngine<MeterReadingCsvDTO>();
+            var meterReadings = engine.ReadString(csvFileContent);
+
+            logger.LogDebug("Successfully found {totalMeterReadings} in csv file", meterReadings.Length);
+            return meterReadings;
         }
     }
 }
